45b8bb5e4c15d5dc04f4df56b88b06bdc60965dd
peng.li <peng.li@gmail.com>
2016-04-25 15:06:41 +0800

    For Udacity Movie project
    a python program to create a html which show a movie title, image and
    youtube url

---
+++ center.py
@@ -0,0 +1,23 @@
+import movie
+import fresh_tomatoes
+
+DeadPool = movie.MovieInfo(
+	"DeadPool",
+	"2016 American superhero film based on the Marvel Comics character of the same name",
+	"https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+	"https://www.youtube.com/watch?v=gtTfd6tISfw")
+
+Captain_America = movie.MovieInfo(
+	"Captain America: Civil War",
+	"Political interference in the Avengers' activities causes a rift between former allies Captain America and Iron Man",
+	"https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
+	"https://www.youtube.com/watch?v=1L3c17AmCZw")
+
+Batman_Superman = movie.MovieInfo(
+	"Batman v Superman: Dawn of Justice",
+	"2016 American superhero film featuring the DC Comics characters Batman and Superman",
+	"https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
+	"https://www.youtube.com/watch?v=fis-9Zqu2Ro")
+
+movies = [DeadPool,Captain_America,Batman_Superman]
+fresh_tomatoes.open_movies_page(movies)
---
+++ movie.py
@@ -0,0 +1,8 @@
+class MovieInfo():
+	def __init__(self, title, summary, poster_image_url, trailer_youtube_url) :
+		self.title = title
+		self.summary = summary
+		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailer_youtube_url
+
+
